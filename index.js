ea57639fd38ef217606c7d3f99df6b7de7ebe3b9
Bimal Saggu <bimalsaggu28@gmail..com>
2023-10-24 12:25:12 +0530

    Handle HTTP GET Requests

--- index.js
+++ index.js
@@ -2,6 +2,23 @@ const express = require('express');
 
 const app = express();
 
+const genres = [
+    {id: 1, name: 'Action'},
+    {id: 2, naem: 'Adventure'},
+    {id: 3, name: 'Comedy'},
+    {id: 4, name: 'Crime'},
+    {id: 5, name: 'Drama'},
+    {id: 6, name: 'Horror'},
+    {id: 7, name: 'Romance'},
+    {id: 8, name: 'Sci-Fi'},
+    {id: 9, name: 'Thriller'},
+    {id: 10, name: 'Violence'}
+];
+
+app.get('/api/genres', (req, res) => {
+    res.send(genres);
+})
+
 app.get('/', (req, res) => {
     res.send('VIDLY APPLICATION');
 });
