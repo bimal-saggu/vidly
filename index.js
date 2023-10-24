b412103a0952c51e67a44171c215259a43a3c668
Bimal Saggu <bimalsaggu28@gmail..com>
2023-10-24 12:38:23 +0530

    Handle HTTP POST Request

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
+const Joi = require('joi');
 const express = require('express');
 
 const app = express();
+
+app.use(express.json());
 
 const genres = [
     {id: 1, name: 'Action'},
@@ -19,9 +22,31 @@ app.get('/api/genres', (req, res) => {
     res.send(genres);
 })
 
+app.post('/api/genres', (req, res) => {
+    const {error} = validateGenre(req.body);
+
+    if(error) return res.status(400).send(error.details[0].message);
+
+    const genre = {
+        id: genres.length + 1,
+        name: req.body.name
+    };
+
+    genres.push(genre);
+    res.send(genres);
+})
+
 app.get('/', (req, res) => {
     res.send('VIDLY APPLICATION');
 });
+
+function validateGenre(genre) {
+    const schema = {
+        name: Joi.string().min(3).required()
+    };
+
+    return Joi.validate(genre, schema);
+}
 
 const port = process.env.PORT || 3000;
 
